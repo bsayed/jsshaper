c50f45b42bdfc3128fecfb9becc59d37fa73b158
Bassam Sayed <bassam@ece.uvic.ca>
2014-10-31 16:04:22 -0700

    added new test file for nested objects and arrays

---
+++ src/testFiles/nestedObjArrTestFile.js
@@ -0,0 +1,25 @@
+function#999 foo()
+{
+    var x = function () {
+        var bar = "asdfasdf"#1000;
+        var z = {x: [1, 2#44, {func: {ff: foo}, arr: [bar]}, foo
+        ],
+        y:{
+            t:true#15, f
+        :
+            {
+                func:{
+                    ff:foo
+                }
+            ,
+                bar:44#9
+            }
+        }
+        ,
+        10
+        :
+        true
+        };
+    };
+}
+//var a = [1,2#44,{func:{ff:foo}},foo];
