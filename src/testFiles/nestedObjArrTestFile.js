da9c57d10311f9562c92758ffa55cabc02a46613
Bassam Sayed <bassam@ece.uvic.ca>
2014-11-02 12:37:39 -0800

    Added support for for-loop rewriting and fixed an issue with nested code blocks

--- src/testFiles/nestedObjArrTestFile.js
+++ src/testFiles/nestedObjArrTestFile.js
@@ -2,11 +2,10 @@ function#999 foo()
 {
     var x = function () {
         var bar = "asdfasdf"#1000;
-        var z = {x: [1, 2#44, {func: {ff: foo}, arr: [bar]}, foo
-        ],
+        var z = {x: [1, 2#44, {func: {ff: foo}, arr: [bar]}, foo],
         y:{
-            t:true#15, f
-        :
+            t:true#15,
+            f:
             {
                 func:{
                     ff:foo
