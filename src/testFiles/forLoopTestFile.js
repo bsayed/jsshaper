bbe0e831b80577767df4b4ba703a56e9220d51a6
Bassam Sayed <bassam@ece.uvic.ca>
2014-11-06 15:23:53 -0800

    Added while-loop test file and support. Modified For-loop test file. Added support for indexing expression e.g. a[i][k].

--- src/testFiles/forLoopTestFile.js
+++ src/testFiles/forLoopTestFile.js
@@ -1,3 +1,11 @@
-for(var i ={x:0#33},y; foo(i.x); i.x++,y={}) {
-}
+// FOR loop examples
+//for(j,k; foo(k);k++){}
+
+for(var a,b; a<a[i]['b'] && i>=foo(k);a++,b++){}
+//for(var i=88,j=99; i<10;i--){}
+
+// FOR_IN loop examples
+//for(j in obj){}
+//for(var i=0 in obj) {}
+//for(var j in obj) {}
 
---
+++ src/testFiles/whileLoopTestFile.js
@@ -0,0 +1,10 @@
+do {
+    var i,y={};
+}while(i);
+
+while(j){
+
+}
+
+var a,b;
+
