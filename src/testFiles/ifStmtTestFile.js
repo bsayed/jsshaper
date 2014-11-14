438c60f6c5aea6a71392d3a8f4de383951991d1d
Bassam Sayed <bassam@ece.uvic.ca>
2014-11-14 11:37:42 -0800

    Added If Statement support and test file.

--- src/testFiles/ifStmtTestFile.js
+++ src/testFiles/ifStmtTestFile.js
@@ -1,13 +1,13 @@
 // throw examples
-
-if(x) {
-    if (y > z)
+function f() {
+    if(x) {
         var x = 33#2;
-else
-    {
-        var x = [false#33
-    ]
-        ;
+    } else {
+        var x = [false#33];
+        if (y > z) {
+            throw x;
+        }
     }
+    throw y;
 }
 //throw function f(){}
