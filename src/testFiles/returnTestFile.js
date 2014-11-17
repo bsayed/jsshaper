e66ed2c2457d265a04c19514114fc6fac86cba83
Bassam Sayed <bassam@ece.uvic.ca>
2014-11-16 20:05:53 -0800

    Added support for try-catch and return statements along with support files.

---
+++ src/testFiles/returnTestFile.js
@@ -0,0 +1,10 @@
+// try-catch-finally examples
+
+function#44 f(){
+    return
+}
+
+function b(){
+    return {arr:[1#1,f]};
+}
+
---
+++ src/testFiles/tryCatchFinallyTestFile.js
@@ -0,0 +1,16 @@
+// try-catch-finally examples
+
+try{
+
+    try{
+
+    } finally {
+
+    }
+
+}catch (x) {
+    console.log("bla bla");
+}finally{
+
+}
+
